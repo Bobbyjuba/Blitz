7bb29f2ce7bc836bcdf7048066a7c8f536bdf708
Brian Roden <32974706+Bobbyjuba@users.noreply.github.com>
2020-05-25 20:14:52 -0500

    Initial Setup
    Created a Windows Form Application for Blitz as well as included the initial assets for the game, including the moving background and gold coin PNGs.

---
+++ Blitz/Blitz/BlitzForm.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Blitz {
+	partial class BlitzForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.SuspendLayout();
+			// 
+			// BlitzForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(434, 561);
+			this.MaximizeBox = false;
+			this.Name = "BlitzForm";
+			this.Text = "Blitz";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
+
