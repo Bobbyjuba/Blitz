4a3c497fa10a19d82eeb75ccb9de1c15face0e41
Brian Roden <Bobbyjuba@gmail.com>
2020-05-26 16:12:05 -0500

    Added car movement, with placeholder carBox. Added moving coin with functionality for disappearing if caught.

--- Blitz/Blitz/BlitzForm.Designer.cs
+++ Blitz/Blitz/BlitzForm.Designer.cs
@@ -27,6 +27,10 @@
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlitzForm));
 			this.gameTimer = new System.Windows.Forms.Timer(this.components);
 			this.backgroundTimer = new System.Windows.Forms.Timer(this.components);
+			this.coinBox = new System.Windows.Forms.PictureBox();
+			this.carBox = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.coinBox)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.carBox)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// gameTimer
@@ -40,18 +44,43 @@
 			this.backgroundTimer.Interval = 65;
 			this.backgroundTimer.Tick += new System.EventHandler(this.backgroundTimer_Tick);
 			// 
+			// coinBox
+			// 
+			this.coinBox.BackColor = System.Drawing.Color.Transparent;
+			this.coinBox.BackgroundImage = global::Blitz.Properties.Resources.coin;
+			this.coinBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.coinBox.Location = new System.Drawing.Point(112, 45);
+			this.coinBox.Name = "coinBox";
+			this.coinBox.Size = new System.Drawing.Size(26, 22);
+			this.coinBox.TabIndex = 0;
+			this.coinBox.TabStop = false;
+			// 
+			// carBox
+			// 
+			this.carBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.carBox.Location = new System.Drawing.Point(150, 485);
+			this.carBox.Name = "carBox";
+			this.carBox.Size = new System.Drawing.Size(28, 45);
+			this.carBox.TabIndex = 1;
+			this.carBox.TabStop = false;
+			// 
 			// BlitzForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-			this.ClientSize = new System.Drawing.Size(582, 703);
+			this.ClientSize = new System.Drawing.Size(436, 571);
+			this.Controls.Add(this.carBox);
+			this.Controls.Add(this.coinBox);
 			this.DoubleBuffered = true;
-			this.Margin = new System.Windows.Forms.Padding(4);
 			this.MaximizeBox = false;
 			this.Name = "BlitzForm";
 			this.Text = "Blitz";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BlitzForm_KeyDown);
+			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BlitzForm_MouseMove);
+			((System.ComponentModel.ISupportInitialize)(this.coinBox)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.carBox)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -60,6 +89,8 @@
 
 		private System.Windows.Forms.Timer gameTimer;
 		private System.Windows.Forms.Timer backgroundTimer;
+		private System.Windows.Forms.PictureBox coinBox;
+		private System.Windows.Forms.PictureBox carBox;
 	}
 }
 
--- Blitz/Blitz/BlitzForm.cs
+++ Blitz/Blitz/BlitzForm.cs
@@ -24,6 +24,7 @@ namespace Blitz {
 
 		// Indexer to iterate through background[]
 		private int index = 0;
+		private int coins = 0;
 
 		public BlitzForm() {
 			InitializeComponent();
@@ -32,7 +33,16 @@ namespace Blitz {
 
 		// gameTimer should handle the majority of event logic
 		private void gameTimer_Tick(object sender, EventArgs e) {
+			if (coinBox.Bounds.IntersectsWith(carBox.Bounds)) {
+				coinBox.Visible = false;
+				coins++;
+			}
 
+			if (coinBox.Location.Y > carBox.Bottom + 5) {
+				coinBox.Visible = false;
+			}
+
+			coinBox.Top += 17;
 		}
 
 		// backgroundTimer should handle the task of displaying the 7 images every 65ms 
@@ -48,6 +58,30 @@ namespace Blitz {
 
 			this.BackgroundImage = background[index];
 			index++;
+		}
+
+		private void BlitzForm_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.A) {
+				if ((carBox.Location.X - (carBox.Width / 2)) >= 53)
+					carBox.Left = carBox.Left - (carBox.Width / 2);
+
+				else if (carBox.Location.X - (carBox.Width / 2) < 53)
+					carBox.Left = carBox.Left - (carBox.Left - 53);
+			}
+
+			if (e.KeyCode == Keys.D) {
+				if (carBox.Left + (carBox.Width / 2) <= 350)
+					carBox.Left = carBox.Left + (carBox.Width / 2);
+
+				else if (carBox.Left + (carBox.Width / 2) > 350)
+					carBox.Left = carBox.Left + (carBox.Width) + (350 - carBox.Right);
+			}
+		}
+
+		private void BlitzForm_MouseMove(object sender, MouseEventArgs e) {
+			if (e.X >= (53 + (carBox.Width / 2)) && e.X <= (350 + (carBox.Width / 2))) {
+				carBox.Left = e.X - (carBox.Width / 2);
+			}
 		}
 	}
 }
